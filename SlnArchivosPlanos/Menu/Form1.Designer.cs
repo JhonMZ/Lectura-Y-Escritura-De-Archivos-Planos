0021856089e0c9adc488301b20fd47c698421f07
JhonMZ <dj.jhon306@hotmail.com>
2015-02-25 01:23:16 -0500

    V1

---
+++ SlnArchivosPlanos/Menu/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Menu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncrear = new System.Windows.Forms.Button();
+            this.btncargar = new System.Windows.Forms.Button();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Nombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Edad = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Ficha = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btncrear
+            // 
+            this.btncrear.Location = new System.Drawing.Point(12, 12);
+            this.btncrear.Name = "btncrear";
+            this.btncrear.Size = new System.Drawing.Size(104, 23);
+            this.btncrear.TabIndex = 0;
+            this.btncrear.Text = "Crear Archivo";
+            this.btncrear.UseVisualStyleBackColor = true;
+            this.btncrear.Click += new System.EventHandler(this.btncrear_Click);
+            // 
+            // btncargar
+            // 
+            this.btncargar.Location = new System.Drawing.Point(318, 12);
+            this.btncargar.Name = "btncargar";
+            this.btncargar.Size = new System.Drawing.Size(90, 23);
+            this.btncargar.TabIndex = 1;
+            this.btncargar.Text = "Cargar Archivo";
+            this.btncargar.UseVisualStyleBackColor = true;
+            this.btncargar.Click += new System.EventHandler(this.btncargar_Click);
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.Location = new System.Drawing.Point(12, 255);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(75, 23);
+            this.btnguardar.TabIndex = 2;
+            this.btnguardar.Text = "Guardar";
+            this.btnguardar.UseVisualStyleBackColor = true;
+            this.btnguardar.Click += new System.EventHandler(this.btnguardar_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Nombre,
+            this.Edad,
+            this.Ficha});
+            this.listView1.Location = new System.Drawing.Point(12, 114);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(396, 135);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 51);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(396, 57);
+            this.textBox1.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Nombre
+            // 
+            this.Nombre.Text = "Nombre";
+            // 
+            // Edad
+            // 
+            this.Edad.Text = "Edad";
+            // 
+            // Ficha
+            // 
+            this.Ficha.Text = "Ficha";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 286);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnguardar);
+            this.Controls.Add(this.btncargar);
+            this.Controls.Add(this.btncrear);
+            this.Name = "Form1";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncrear;
+        private System.Windows.Forms.Button btncargar;
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ColumnHeader Nombre;
+        private System.Windows.Forms.ColumnHeader Edad;
+        private System.Windows.Forms.ColumnHeader Ficha;
+    }
+}
+
---
+++ SlnArchivosPlanos/Menu/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+//Biblioteca de clases para utilizar StramReader y StreamWriter
+using System.IO;
+
+namespace Menu
+{
+    public partial class Form1 : Form
+    {
+        string dirarchivo = null;//variable para obtener la direccion del archivo
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncrear_Click(object sender, EventArgs e)
+        {
+
+            saveFileDialog1.Filter = "Text File | *.txt"; //filtro de archivos txt 
+            saveFileDialog1.DefaultExt = "txt"; //extencion por defecto txt
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string nombrearchivo = saveFileDialog1.FileName; //obtiene nombre del archivo
+                StreamWriter sw = File.CreateText(nombrearchivo); //crea el archivo con el nombre que se le ponga
+
+                sw.Close();
+            }
+        }
+
+        private void btncargar_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear(); //Limpia todos los items del listview
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                dirarchivo = openFileDialog1.FileName;
+                StreamReader sr = new StreamReader(dirarchivo);
+
+                while(!sr.EndOfStream)//Ciclo hasta terminar las lineas o filas del archivo
+                {
+                    string strlinea = sr.ReadLine();//lee y asigna el texto de la linea en la variable 
+
+                    if (strlinea.Length > 0)//condicion para saber si la linea o fila contiene texto
+                    {
+                        int intpos = strlinea.IndexOf(",");//obtiene la posicion de la coma 
+                        string nombre = strlinea.Substring(0, intpos);//obtiene una cadena desde la posicion 0 hasta la coma
+
+                        strlinea = strlinea.Substring(intpos + 1);//corta la cadena despues de la coma
+
+                        intpos = strlinea.IndexOf(",");
+                        int edad = Convert.ToInt32(strlinea.Substring(0, intpos));
+
+                        strlinea = strlinea.Substring(intpos + 1);
+
+                        intpos = strlinea.IndexOf(",");
+                        int ficha = Convert.ToInt32(strlinea);
+
+                        ListViewItem item = new ListViewItem();//objeto de items para agregar en el listview
+
+                        item.Text = nombre;//texto del item o columna 1
+
+                        item.SubItems.Add(edad.ToString());//columna 2
+                        item.SubItems.Add(ficha.ToString());//columna 3
+
+                        listView1.Items.Add(item);//agregamos el item al listview
+                    }
+                }
+                sr.Close();
+            }
+        }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = File.AppendText(dirarchivo);//Se abre el archivo y se posiciona al final
+
+            string[] lineas = textBox1.Lines;//se obtiene todo el contenido del texbox
+
+            for (int i = 0; i < lineas.Length; i++)//ciclo para escribir en el archivo
+            {
+                sw.WriteLine(lineas[i]);//escribe el contenido del vector linea por linea o fila por fila en el archivo
+            }
+
+            sw.Close();
+        }
+    }
+}
